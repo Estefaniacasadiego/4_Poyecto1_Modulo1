d6058a9bfc893f2cf384368df284674fa95f8376
JuanPabloAndres1 <juan.hernandez.orbegozo@gmail.com>
2023-10-05 22:26:01 -0500

    Se integra el chat interactivo con chat GPT

---
+++ API.js
@@ -0,0 +1,16 @@
+
+/*const URL="https://api.openai.com/v1/chat/completions"
+const MODEL="gpt-3.5-turbo"
+const TOKEN= "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"
+
+let body= {
+model: MODEL, 
+messages:[{role: "user", content: "dame los 5 lugares mas turisticos de Colombia"}]
+}
+let header={
+    "Authorization":"Bearer " + TOKEN, 
+    "Content-Type": "application/json" 
+}
+fetch(URL, {method: "POST", headers: header, body: JSON.stringify(body)}).then(res=>{
+    console.log(res)
+})*/
